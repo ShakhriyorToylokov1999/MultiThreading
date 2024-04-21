d9c3c264919fd246de7849f96bad9626c5bfa9a6
ShakhriyorToylokov1999 <118186892+ShakhriyorToylokov1999@users.noreply.github.com>
2024-04-21 23:56:43 +0500

    Task 5 completed

--- MultiThreading.Task5.Threads.SharedCollection/Program.cs
+++ MultiThreading.Task5.Threads.SharedCollection/Program.cs
@@ -5,11 +5,17 @@
  * Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.
  */
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task5.Threads.SharedCollection
 {
     class Program
     {
+        static List<int> collection = new List<int>();
+        static object lockObject = new object();
+
         static void Main(string[] args)
         {
             Console.WriteLine("5. Write a program which creates two threads and a shared collection:");
@@ -17,9 +23,48 @@ namespace MultiThreading.Task5.Threads.SharedCollection
             Console.WriteLine("Use Thread, ThreadPool or Task classes for thread creation and any kind of synchronization constructions.");
             Console.WriteLine();
 
-            // feel free to add your code
+            Console.WriteLine("The array elements are being added: ");
+            Task task1 = Task.Factory.StartNew(() => Add());
+            Task task2 = Task.Factory.StartNew(() => Print());
 
+            Task.WaitAll(task1, task2);
+
+            Console.WriteLine("Array has been completed ");
             Console.ReadLine();
         }
+
+        static void Add()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                lock (lockObject)
+                {
+                    collection.Add(i);
+                    Monitor.Pulse(lockObject); 
+                }
+                Thread.Sleep(100);
+            }
+        }
+
+        static void Print()
+        {
+            lock (lockObject)
+            {
+                while (collection.Count < 10)
+                {
+                    Monitor.Wait(lockObject);
+                }
+            }
+
+            for (int i = 1; i <= 10; i++)
+            {
+                lock (lockObject)
+                {
+                    Console.WriteLine($"[{string.Join(", ", collection.GetRange(0, i))}]");
+                }
+                Thread.Sleep(100);
+            }
+        }
+
     }
 }
