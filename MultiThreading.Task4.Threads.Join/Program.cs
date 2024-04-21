ff19737c331bc2f1c1870aa8f9815277d00d750b
ShakhriyorToylokov1999 <118186892+ShakhriyorToylokov1999@users.noreply.github.com>
2024-04-21 23:25:33 +0500

    Task 4 completed

--- MultiThreading.Task4.Threads.Join/Program.cs
+++ MultiThreading.Task4.Threads.Join/Program.cs
@@ -10,11 +10,14 @@
  */
 
 using System;
+using System.Threading;
 
 namespace MultiThreading.Task4.Threads.Join
 {
     class Program
     {
+        static readonly Semaphore semaphore = new Semaphore(0, 10);
+
         static void Main(string[] args)
         {
             Console.WriteLine("4.	Write a program which recursively creates 10 threads.");
@@ -24,11 +27,51 @@ namespace MultiThreading.Task4.Threads.Join
             Console.WriteLine("- a) Use Thread class for this task and Join for waiting threads.");
             Console.WriteLine("- b) ThreadPool class for this task and Semaphore for waiting threads.");
 
-            Console.WriteLine();
+            Console.WriteLine("\n Enter a number to start decrement: ");
+            int inputNumber;
+            int.TryParse(Console.ReadLine(), out inputNumber);
 
-            // feel free to add your code
+            Console.WriteLine("Task a: Using Thread class and Join: ");
+            Thread thread = new Thread(() => DecrementAndPrint(inputNumber));
+            thread.Start();
+            thread.Join();
+
+            Console.WriteLine("\n Task b: Using ThreadPool and Semaphore:");
+            ThreadPool.QueueUserWorkItem(new WaitCallback(DecrementAndPrintWithSemaphore), inputNumber);
+            semaphore.WaitOne();
 
             Console.ReadLine();
         }
+
+
+        private static void DecrementAndPrint(int state)
+        {
+            Console.WriteLine($"{state} -  Thread Id {Thread.CurrentThread.ManagedThreadId} ");
+            state--;
+
+            if (state > 0)
+            {
+                Thread thread = new Thread(() => DecrementAndPrint(state));
+                thread.Start();
+                thread.Join();
+            }
+        }
+
+        private static void DecrementAndPrintWithSemaphore(object state)
+        {
+            int value = (int)state;
+            Console.WriteLine($"{value} - Thread Id {Thread.CurrentThread.ManagedThreadId}");
+            value--;
+
+            if (value > 0)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(DecrementAndPrintWithSemaphore), value);
+            }
+            else
+            {
+                semaphore.Release();
+            }
+        }
+
     }
 }
