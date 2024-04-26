933f29eef10f198554e00233be772e9366680125
ShakhriyorToylokov1999 <118186892+ShakhriyorToylokov1999@users.noreply.github.com>
2024-04-27 00:01:17 +0500

    Task 6 completed

--- MultiThreading.Task6.Continuation/Program.cs
+++ MultiThreading.Task6.Continuation/Program.cs
@@ -7,6 +7,8 @@
    Demonstrate the work of the each case with console utility.
 */
 using System;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MultiThreading.Task6.Continuation
 {
@@ -22,9 +24,48 @@ namespace MultiThreading.Task6.Continuation
             Console.WriteLine("Demonstrate the work of the each case with console utility.");
             Console.WriteLine();
 
-            // feel free to add your code
+            Task parentTask = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("Parent task");
+                throw new Exception("Simulating exception foe Parent task have a failure");
+            });
+
+            // Task a)
+            Task task1 = parentTask.ContinueWith((task) =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("Continuation task 1: Executed regardless of the result of the parent task");
+                
+            });
+
+            // Task b) 
+            Task task2 = parentTask.ContinueWith((task) =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("Continuation task 2: Executed when the parent task was finished without success");
+             
+            }, TaskContinuationOptions.OnlyOnFaulted);
+
+            // Task c)
+            Task task3 = parentTask.ContinueWith((task) =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("Continuation task 3: Parent task thread has been reused for continuation: ");
+            }, TaskContinuationOptions.AttachedToParent);
+
+            // Task d)
+            Task task4 = parentTask.ContinueWith((task) =>
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine("Continuation task 4: Executed outside of the thread pool when the parent task is cancelled");
+            }, TaskContinuationOptions.LongRunning | TaskContinuationOptions.NotOnCanceled);
+
+            Task.WaitAll(task1, task2, task3, task4);
+
+            Console.WriteLine("Tasks have been completed");
 
             Console.ReadLine();
         }
+
     }
 }
